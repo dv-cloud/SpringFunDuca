d5edd89fcd97974f9691f60682515fdcbdda7a0b
dv-cloud <duca.vladislav@gmail.com>
2020-03-30 21:41:39 +0300

    Use config file

---
+++ src/main/java/com/example/demo/SelfEmployed.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SelfEmployed implements Worker {
+    private Employee contractor;
+    private String name;
+
+    public SelfEmployed(Employee contractor, String name) {
+        this.contractor = contractor;
+        this.name = name;
+    }
+
+
+    public SelfEmployed(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getPosition() {
+        return "Self-Employed";
+    }
+
+    public Employee getContractor() {
+        return contractor;
+    }
+
+    public void setContractor(Employee contractor) {
+        this.contractor = contractor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/demo/SpringFunDucaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFunDucaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFunDucaApplication.class, args);
+    }
+
+}
